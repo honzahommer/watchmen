7dc7a462d2b1cb8c559b20c7909ef06d064c7331
Honza Hommer <honza@hommer.cz>
2017-11-28 16:45:06 +0100

    COMMIT ALL THE FILES!

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,6 @@
 const pkg = require('./package.json');
 const path = require('path');
 
-const _env = [];
-const _env_production = [];
-
 const env = {
   NODE_ENV: 'development',
   WATCHMEN_BASE_URL: 'http://localhost/' + pkg.name  + '/',
@@ -19,24 +16,15 @@ const env_production = {
   WATCHMEN_AUTH_NODEMAILER_SENDMAIL: 'true'
 }
 
-for (var key in env) {
-  _env.push('export ' + key + '=' + env[key] + ' && ');
-  _env_production.push('export ' + key + '=' + env[key] + ' && ');
-}
-
-for (var key in env_production) {
-  _env_production.push('export ' + key + '=' + env_production[key] + ' && ');
-}
-
 module.exports = {
   apps : [{
     name: pkg.name + '-monitor-server',
     script: 'run-monitor-server.js',
-    env, env_production
+    env
   }, {
     name: pkg.name + '-web-server',
     script: 'run-web-server.js',
-    env, env_production
+    env
   }],
   deploy: {
     production: {
@@ -44,7 +32,8 @@ module.exports = {
       host: '192.168.200.42',
       ref: 'origin/master',
       repo: pkg.repository.url,
-      path: '/usr/local/app/' + pkg.name
+      path: '/usr/local/app/' + pkg.name,
+      env: env_production
     }
   }
 };
