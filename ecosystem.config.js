fe60d1318844c3790342c135f6f77e68600e7925
Honza Hommer <honza@hommer.cz>
2017-11-28 15:16:03 +0100

    The same thing we do every night, Pinky - try to take over the world!

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,27 +1,31 @@
 const pkg = require('./package.json');
 const path = require('path');
 
+const env = {
+  NODE_ENV: 'development',
+  WATCHMEN_BASE_URL: 'http://localhost/' + pkg.name  + '/',
+  WATCHMEN_WEB_NO_AUTH: 'true',
+  WATCHMENT_AUTH_NODEMAILER_NO_AUTH: 'true'
+}
+
+const env_production = {
+  NODE_ENV: 'production',
+  WATCHMEN_BASE_URL: 'https://app.opennet.cz/' + pkg.name  + '/',
+  WATCHMEN_WEB_PORT: '3100',
+  WATCHMEN_AUTH_NODEMAILER_USER: 'root@cap-net.cz',
+  WATCHMEN_AUTH_NODEMAILER_SENDMAIL: 'true',
+  WATCHMEN_MAILER_TEMPLATE_DIRECTORY: path.join(__dirname, 'webserver/views/nodemailer')
+}
+
 module.exports = {
   apps : [{
-    name: 'watchmen-monitor-server',
-    script: 'run-monitor-server.js'
+    name: pkg.name + '-monitor-server',
+    script: 'run-monitor-server.js',
+    env, env_production
   }, {
-    name: 'watchmen-web-server',
+    name: pkg.name + '-web-server',
     script: 'run-web-server.js',
-    env: {
-      NODE_ENV: 'development',
-      WATCHMEN_BASE_URL: 'http://localhost/' + pkg.name  + '/',
-      WATCHMEN_WEB_NO_AUTH: 'true',
-      WATCHMENT_AUTH_NODEMAILER_NO_AUTH: 'true'
-    },
-    env_production: {
-      NODE_ENV: 'production',
-      WATCHMEN_BASE_URL: 'https://app.opennet.cz/' + pkg.name  + '/',
-      WATCHMEN_WEB_PORT: '3100',
-      WATCHMEN_AUTH_NODEMAILER_USER: 'root@cap-net.cz',
-      WATCHMEN_AUTH_NODEMAILER_SENDMAIL: 'true',
-      WATCHMEN_MAILER_TEMPLATE_DIRECTORY: path.join(__dirname, 'webserver/views/nodemailer')
-    }
+    env, env_production
   }],
   deploy: {
     production: {
@@ -31,9 +35,7 @@ module.exports = {
       repo: pkg.repository.url,
       path: '/usr/local/app/' + pkg.name,
       'post-deploy': 'npm install && bower --allow-root install && gulp build && pm2 startOrGracefulReload ecosystem.config.js --env production',
-      env: {
-        NODE_ENV: 'production'
-      }
+      env: env_production
     }
   }
 };
